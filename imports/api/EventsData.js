513dc99b13d04362892eee745127097c6bb6faa2
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-04-02 22:25:46 -0500

    Update EventsData.js

---
+++ imports/api/EventsData.js
@@ -0,0 +1,4 @@
+/*
+It is important to be careful with all the js classes in a meteor porject, since meteor execute them all.
+If it wasn't used or is part of an "on-going" process, it shouldn't be uploaded to the master branch
+*/
